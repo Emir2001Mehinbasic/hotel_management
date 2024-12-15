dc9df792e4f7c2f513e3715caa34eae64c163966
Maid Kurtic <kurticmaid@gmail.com>
2024-12-15 23:54:16 +0100

    Implementacija metoda osim izdavanja racuna

--- index.js
+++ index.js
@@ -25,8 +25,12 @@ class Osoba {
 class Admin {
     maxBrojKorisnika = 50;
     gosti = [];
-    constructor(ime) {
-        
+    logovaniGosti = [];
+    id;
+    username;
+    constructor(id = 'admin', usernam = 'adminadmin') {
+        this.id = id;
+        this.username = username;
     }
     korisnickoIme(ime, prezime) {
       return `${ime}_${prezime}`;
@@ -46,12 +50,18 @@ class Admin {
            brojSobe: brojSobe,
            tipSobe: tipSobe,
            vrijeme_prijave: Date(),
-           korisnickoIme : korisnickoIme(korisnik.ime, korisnik.prezime),
-           password: korisnickiPassword(korisnik.ime, korisnik.prezime, korisnik.godine),
-           dodatne_usluge : []
+           korisnickoIme : this.korisnickoIme(korisnik.ime, korisnik.prezime),
+           password: this.korisnickiPassword(korisnik.ime, korisnik.prezime, korisnik.godine),
+           dodatneUsluge : []
         };
         this.gosti.push(gost)
+    }
 
+    logovanjeGosta(korisnickoIme, sifra) {
+        let logovan = this.gosti.find(e => e.korisnickoIme === korisnickoIme && e.password == sifra) 
+        if(logovan) {
+            this.logovaniGosti.push(logovan);
+        }
     }
     promijeniTipSobe (ime,prezime, tipSobe) {
         let gost = this.gosti.find(gost => gost.ime === ime && gost.prezime === prezime)
@@ -68,7 +78,7 @@ class Admin {
         gost.brojSobe = brojSobe;
        }
     }
-    promijeniUslugu (ime,prezime, usluga) {
+    dodajUslugu (ime,prezime, usluga) {
         let gost = this.gosti.find(gost => gost.ime === ime && gost.prezime === prezime)
 
        if (gost) {
@@ -76,10 +86,38 @@ class Admin {
        }
     }
 
-    odjavaKorisnika(gost) {
+    izbrisiUslugu(ime,prezime,usluga) {
         let gost = this.gosti.find(gost => gost.ime === ime && gost.prezime === prezime)
+        let imaUslugu = gost.dodatneUsluge.indexOf(usluga);
+        if(gost && imaUslugu >= 0) {
+            gost.dodatneUsluge.splice(imaUslugu, 1)
+        }
+    }
+
+    odjavaKorisnika(korisnik) {
+        let gost = this.gosti.find(e => e.ime === korisnik.ime && e.prezime === korisnik.prezime)
         if(gost) {
             this.gosti = this.gosti.filter(e => e != gost)
         }
+    }
+
+    sviKorisnicni() {
+        return this.gosti;
+    }
+    
+    izlogujSveKorisnike() {
+        this.logovaniGosti.length = 0;
+    }
+    
+    izlogujKorisnika(gost) {
+        let logovan = this.logovaniGosti.find(e => e.ime === gost.ime && e.prezime === gost.prezime)
+        if(logovan) {
+        this.logovaniGosti = this.logovaniGosti.filter(e => e.ime !== gost.ime)
+           }   
+         }
+
+    gasenjeSistema() {
+        this.gosti = [];
+        this.logovaniGosti = [];
     }
 }
---
+++ test.js
@@ -0,0 +1,3 @@
+arr = [1,2,3,4];
+arr.length = 0;
+console.log(arr)
